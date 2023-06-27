85f9401e06f9f686ecc1b758b0c1f0f4a67e12c5
Joao Ponte <joao_ponte@msn.com>
2023-06-27 11:06:00 +0100

    add MapKit

--- OnTheMap/Controller/MapStudentController.swift
+++ OnTheMap/Controller/MapStudentController.swift
@@ -6,7 +6,18 @@
 //
 
 import UIKit
+import MapKit
 
-class MapStudentController: UIViewController {
+class MapStudentController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapVIew: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        <#code#>
+//    }
 }
