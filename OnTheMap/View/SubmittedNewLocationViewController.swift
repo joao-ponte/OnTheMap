9ce179054b3c3b2dada3f1680589dd1f38835c9f
Joao Ponte <joao_ponte@msn.com>
2023-06-29 16:50:32 +0100

    Add activity indicator for loading state during location submission.

--- OnTheMap/View/SubmittedNewLocationViewController.swift
+++ OnTheMap/View/SubmittedNewLocationViewController.swift
@@ -12,6 +12,7 @@ class SubmittedNewLocationViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     let textFieldDelegate = TextFieldDelegate()
     var locationText: String?
@@ -74,31 +75,41 @@ class SubmittedNewLocationViewController: UIViewController, MKMapViewDelegate {
               let longitude = longitude else {
             return
         }
-
+        
+        activityIndicator.startAnimating() // Start activity indicator
+        
         let existingStudent = StudentModel.students.first { $0.objectId == OTMClient.Auth.objectId }
 
         if existingStudent != nil {
             // Update existing student's location
-            OTMClient.updateStudent(mapString: location, mediaURL: mediaURL, latitude: Float(latitude), longitude: Float(longitude)) { success, error in
+            OTMClient.updateStudent(mapString: location, mediaURL: mediaURL, latitude: Float(latitude), longitude: Float(longitude)) { [weak self] success, error in
+                guard let self = self else { return }
+                
+                self.activityIndicator.stopAnimating() // Stop activity indicator
+                
                 if success {
                     self.showAlert(title: "Success", message: "The student location was updated successfully", dismissHandler: { [weak self] in
                         self?.dismiss(animated: true)
                     })
                 } else {
-                    let errorMessage = error?.localizedDescription ?? "Unknown error"
+                    let errorMessage = error?.localizedDescription ?? "Try again!"
                     self.showAlert(title: "Weird", message: errorMessage, dismissHandler: { [weak self] in
                         self?.dismiss(animated: true)
                     })
                 }
             }
         } else {
-            OTMClient.addStudent(mapString: location, mediaURL: mediaURL, latitude: Float(latitude), longitude: Float(longitude)) { success, error in
+            OTMClient.addStudent(mapString: location, mediaURL: mediaURL, latitude: Float(latitude), longitude: Float(longitude)) { [weak self] success, error in
+                guard let self = self else { return }
+                
+                self.activityIndicator.stopAnimating() // Stop activity indicator
+                
                 if success {
                     self.showAlert(title: "Success", message: "A student was added successfully", dismissHandler: { [weak self] in
                         self?.dismiss(animated: true)
                     })
                 } else {
-                    let errorMessage = error?.localizedDescription ?? "Unknown error"
+                    let errorMessage = error?.localizedDescription ?? "Try again!"
                     self.showAlert(title: "Weird", message: errorMessage, dismissHandler: { [weak self] in
                         self?.dismiss(animated: true)
                     })
@@ -116,13 +127,12 @@ class SubmittedNewLocationViewController: UIViewController, MKMapViewDelegate {
         present(alert, animated: true)
     }
 
-    
     func centerMapOnLocation(_ coordinate: CLLocationCoordinate2D) {
         let regionRadius: CLLocationDistance = 1000
         let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
+
     @IBAction func cancelNewLocation(_ sender: Any) {
         dismiss(animated: true)
     }
